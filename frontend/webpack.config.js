69636d4f4ade6d5b8666a4904bc760c7f04433c0
Douglas Santiago <douglas.s.rodrigues@ufv.br>
2018-12-29 14:03:26 -0200

    Configurando rotas

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,24 +2,23 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.jsx',
+    entry: './public/src/index.jsx',
     output: {
         path: __dirname + '/public',
         filename: './app.js'
     },
     devServer: {
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
     resolve: {
-        extensions: ['','.js','.jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + 'node_modules'
+            modules: __dirname + '/node_modules'
         }
     },
-
-    plugins: [
-        new ExtractTextPlugin('app.cs')
+    plugins: [ 
+        new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
@@ -27,14 +26,14 @@ module.exports = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['es2015','react'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        },{
-            test:/\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader','css-loader')
-        },{
-            test:/\.woff|.woff2|.tff|.eot|.svg*.*$/,
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
